0e37ce81a64e7a5e8208d73d2de8a0df0e29e44f
B Sridhar <Sridharb.in@gmail.com>
2016-08-09 20:52:27 +0530

    changes

--- src/main/java/com/demo/jsondemo/TestClass.java
+++ src/main/java/com/demo/jsondemo/TestClass.java
@@ -3,18 +3,22 @@ package com.demo.jsondemo;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.EnumSet;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.spi.json.JacksonJsonNodeJsonProvider;
+import com.jayway.jsonpath.spi.json.JacksonJsonProvider;
+import com.jayway.jsonpath.spi.json.JsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+import com.jayway.jsonpath.spi.mapper.MappingProvider;
 
 
 
@@ -25,179 +29,142 @@ public class TestClass {
 	
 	public static final String FOLDER_PATH="D:\\jsondemo\\src\\main\\resources\\";	
 	
-	public static final String PATTERN_STRING="[.]";
-	
-	public static final Pattern pattern= Pattern.compile(PATTERN_STRING);
-	
-	public static final String NEWLINE="\r";
-	
-	public static final String OPEN_CURLY_BRACES="{";
-	
-	public static final String CLOSE_CURLY_BRACES="}";
-	
-	public static final String FORWARD_SLASH="/";
-	
-	public static final String COLON=":";
-	
-	public static final Character DOUBLE_QUOTES='"';
-	
-	public static final String COMMA=",";
 	
 	
-	public String findAndUpdateFieldByKey(String key,JsonNode jsonNode,String repValue){
+	TestClass(){
+		
+		Configuration.setDefaults(new Configuration.Defaults() {
+
+		    private final JsonProvider jsonProvider = new JacksonJsonProvider();
+		    private final MappingProvider mappingProvider = new JacksonMappingProvider();
+
+		    public JsonProvider jsonProvider() {
+		        return jsonProvider;
+		    }
+
+		    public MappingProvider mappingProvider() {
+		        return mappingProvider;
+		    }
+
+		    public Set<Option> options() {
+		        return EnumSet.noneOf(Option.class);
+		    }
+		});
+
+		
+	}
+	
+	
+	public String findAndUpdateFieldByKey(String jsonString, String key,String repValue){
 		
 		LOG.debug("Entering");
-		String fieldNodeStr =null;
+		
+		String fieldNodeStr=null;
 		try {
 			ObjectMapper mapper = new ObjectMapper();
-			JsonNode fieldNode = jsonNode.at(key);
-			((ObjectNode) fieldNode).put(key,repValue);
-			fieldNodeStr = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(fieldNode);
-			
-		} catch (IOException e) {
-			LOG.error("IOException:",e);
-		}catch (Exception e) {
+			Configuration config= Configuration.builder()
+				    .jsonProvider(new JacksonJsonNodeJsonProvider())
+				    .mappingProvider(new JacksonMappingProvider())
+				    .build();
+
+			JsonNode newJson=JsonPath.using(config).parse(jsonString).set("$." +key,repValue).json();
+			fieldNodeStr = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(newJson);
+		} catch (JsonProcessingException e) {
 			LOG.error("Exception:",e);
 		}
-		
 		LOG.debug("Leaving.");
 		return fieldNodeStr;
 	}
 
-private String getFieldByKey(String key,JsonNode jsonNode){
-	
-	LOG.debug("Entering");
+
+public String getValues(String key,String jsonString){
 	String fieldNodeStr =null;
+	LOG.debug("Entering");
+	
 	try {
+		
 		ObjectMapper mapper = new ObjectMapper();
-		JsonNode fieldNode = jsonNode.at(key);
-		fieldNodeStr = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(fieldNode);
+		JsonNode testNode = JsonPath.parse(jsonString)
+                .read("$." + key, JsonNode.class);
+		fieldNodeStr = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testNode);
 		
 	} catch (IOException e) {
 		LOG.error("IOException:",e);
 	}catch (Exception e) {
 		LOG.error("Exception:",e);
 	}
-	
 	LOG.debug("Leaving.");
 	return fieldNodeStr;
 }
 
 
-public String findAndUpdateFieldsByConfig(String fields,String jsonString,String repText){
-	LOG.debug("Entering");
-	StringBuffer strBuf=new StringBuffer(255);
-	ObjectMapper mapper = new ObjectMapper();
-	JsonNode node;
-	String retString=null;
-	
-	strBuf.append(OPEN_CURLY_BRACES).append(NEWLINE);
-	
-	List<String> fieldToIncludeListr=stringToList(fields);
-	
-	try {
-		
-		node = mapper.readTree(jsonString);
-		for(String str:fieldToIncludeListr){
-		       Matcher m = pattern.matcher(str); 
-		       findAndUpdateFieldByKey(FORWARD_SLASH + m.replaceAll(FORWARD_SLASH),node,repText);
-			}
-		
-	} catch (JsonProcessingException e) {
-		LOG.error("JsonProcessingException:" ,e);
-	} catch (IOException e) {
-		LOG.error("IOException:" ,e);
-	}catch (Exception e) {
-		LOG.error("Exception:" ,e);
-	}
-	
-	if(null!=strBuf){
-		
-	retString=strBuf.toString();
-	
-	if(null!=retString){
-		//Remove trailing comma
-		retString = retString.replaceAll(",$", "");
-		//Append a New Line Character
-		retString=retString +  NEWLINE + NEWLINE + CLOSE_CURLY_BRACES;
-	   }
-	}
-	LOG.debug("Leaving");
-	return retString;
-}
+/*
+ * 
+ * Prefix all fields with $.(DONE)
+ * If array field need to suffix [*] (Can be first field are anywhere in-between the hierarchy of fields
+ * 
+ */
 
-
-public String includeFieldsByConfig(String fields,String jsonString){
-		LOG.debug("Entering");
-		StringBuffer strBuf=new StringBuffer(255);
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode node;
-		String retString=null;
-		
-		strBuf.append(OPEN_CURLY_BRACES).append(NEWLINE);
-		
-		List<String> fieldToIncludeListr=stringToList(fields);
-		
-		try {
-			node = mapper.readTree(jsonString);
-			
-			//there should be a better approach than what I have done below... For now, I am doing like this...
-			for(String str:fieldToIncludeListr){
-			       Matcher m = pattern.matcher(str); 
-			       strBuf.append(NEWLINE)
-			       		 .append(DOUBLE_QUOTES)
-			       		 .append(str)
-			       		 .append(DOUBLE_QUOTES)
-			       		 .append(COLON)
-			       		 .append(getFieldByKey(FORWARD_SLASH + m.replaceAll(FORWARD_SLASH),node)).append(COMMA);
-				}
-		} catch (JsonProcessingException e) {
-			LOG.error("JsonProcessingException:" ,e);
-		} catch (IOException e) {
-			LOG.error("IOException:" ,e);
-		}catch (Exception e) {
-			LOG.error("Exception:" ,e);
-		}
-		
-		if(null!=strBuf){
-			
-		retString=strBuf.toString();
-		
-		if(null!=retString){
-			//Remove trailing comma
-			retString = retString.replaceAll(",$", "");
-			//Append a New Line Character
-			retString=retString +  NEWLINE + NEWLINE + CLOSE_CURLY_BRACES;
-		   }
-		}
-		LOG.debug("Leaving");
-		return retString;
-}
 	public static void main(String arg[]){
 		
 		TestClass testObj=new TestClass();
 		try {
-			
-				//Include specific Fields in Json
+		
+				System.out.println("***********START: Demo for Including Selected Fields...");
+				
 				String str = readFile(FOLDER_PATH + "Test_Json.json");
 				
-				LOG.info("START: Testing Fields to Include while logging Payload");
-				LOG.info("Original:" + str);
-				String fieldstoInclude="manager.userId,administrator.isSuperUser,salesId,requester.id";
-				
-				System.out.println(testObj.includeFieldsByConfig(fieldstoInclude,str));
+				System.out.println("Original...");
+				System.out.println(str);
 				
 				
-				//Mask Specific Fields in Json
-				LOG.info("START: Testing Mask Fields in input");
-				String fieldsToMask1="manager.userId";
+				
+				//TODO: Convert requester to $.requester[*] if requester is an array field
+				System.out.println("Including Requester...");
+				System.out.println(testObj.getValues("requester[*]",str));
+				
+				//TODO: Convert manager to $.manager
+				System.out.println("Including Manager...");
+				System.out.println(testObj.getValues("manager",str));
+				
+				
+				//TODO: Convert requester.demographic.firstName to $.requester[*].demographic.firstName
+				System.out.println("Including requester.demographic.firstName...");
+				System.out.println(testObj.getValues("requester[*].demographic.firstName",str));
+				
+				System.out.println("***********END: Demo for Including Selected Fields...");
+				System.out.println("");
+		
+				System.out.println("");
+				System.out.println("***********START: Demo for Masking...");
+			
+				System.out.println("Original...");
+				System.out.println(str);
+				
+				
+				//TODO: Convert administrator.userId to $.administrator.userId
+				String fieldsToMask1="administrator.userId";
 				String maskString="?????";
-				LOG.info("Field to be Masked:" + fieldsToMask1);
+				System.out.println("Masking...administrator.userId as :" + maskString);
+				str=testObj.findAndUpdateFieldByKey(str,fieldsToMask1,maskString);
 				
-				testObj.findAndUpdateFieldsByConfig(fieldsToMask1,str,maskString);
+				System.out.println("Masked...administrator.userId as :" + maskString);
+				System.out.println(str);
 				
-				LOG.info("END: Testing Mask Fields in input");
-				LOG.info("");
+				
+				//TODO: Convert requester.demographic.firstName to $.requester[*].demographic.firstName
+				String firstNameField="requester[*].demographic.firstName";
+				String maskString2="*****";
+				System.out.println("Masking...demographic.firstName as :" + maskString2);
+				System.out.println(testObj.findAndUpdateFieldByKey(str,firstNameField,maskString2));
+				str=testObj.findAndUpdateFieldByKey(str,firstNameField,maskString2);
+				System.out.println("Masked...demographic.firstName as :" + maskString2);
+				System.out.println(str);
+				
+				
+				System.out.println("***********END:Demo for Masking.");
+				System.out.println("");
+				
 				
 		}catch (IOException e) {
 				LOG.error("IOException:",e);
@@ -208,14 +175,7 @@ public String includeFieldsByConfig(String fields,String jsonString){
 	}
 	
 	
-	private static List<String> stringToList(String delimitedString){
-		List<String> newList=new ArrayList<String>();
-		StringTokenizer strtokenizer=new StringTokenizer(delimitedString,COMMA);
-		while(strtokenizer.hasMoreTokens()){
-			newList.add(strtokenizer.nextToken());
-		}
-		return newList;
-	}
+	
 
 		
 	private static String readFile(String path)  throws IOException 
